139ceee6152e45534acfb8a13dcd73cffd589755
mukhulaazam <mukhulaazam@gmail.com>
2019-03-02 12:39:45 +0600

    About page is almost done and next is navigation menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-// About us page section
-
+/*	------------------------------------
+		About us page section
+----------------------------------------   */
 // Skilled bar section started 
 $(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
+	$('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+			width:$(this).attr('data-percent')
 		},3000);
 	});
 });
