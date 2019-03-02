36b4005b875a270982f024a87773799cc6561e94
mukhulaazam <mukhulaazam@gmail.com>
2019-03-02 14:28:29 +0600

    Preloader added into index page and next it fixed it

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-/*	------------------------------------
-		About us page section
-----------------------------------------   */
-// Skilled bar section started 
-$(document).ready(function(){
-	$('.skillbar').each(function(){
-		$(this).find('.skillbar-bar').animate({
-			width:$(this).attr('data-percent')
-		},3000);
+$(document).ready(function() {
+	"use strict";
+	$(window).load(function() {
+		$(".status").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
 	});
+
+	/*	------------------------------------
+			About us page section
+	----------------------------------------   */
+	// Skilled bar section started 
+	$(document).ready(function(){
+		$('.skillbar').each(function(){
+			$(this).find('.skillbar-bar').animate({
+				width:$(this).attr('data-percent')
+			},3000);
+		});
+	});
+	// Skilled bar section end
 });
-// Skilled bar section end
+
