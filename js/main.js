1e76aaaf6b30f1b65988b8f4e9ce28777c08a8a5
mukhulaazam <mukhulaazam@gmail.com>
2019-03-05 17:41:53 +0600

    Navigation menu working globaly and next is fixing the navigation menu color

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	"use strict";
 	$(window).load(function() {
-		$(".status").fadeOut();
+		$(".preloader-container").fadeOut();
 		$(".preloader").delay(1000).fadeOut("slow");
 	});
 
@@ -9,12 +9,10 @@ $(document).ready(function() {
 			About us page section
 	----------------------------------------   */
 	// Skilled bar section started 
-	$(document).ready(function(){
-		$('.skillbar').each(function(){
-			$(this).find('.skillbar-bar').animate({
-				width:$(this).attr('data-percent')
-			},3000);
-		});
+	$('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+			width:$(this).attr('data-percent')
+		},3000);
 	});
 	// Skilled bar section end
 });
