0e4691f6fbcc0434da1611c88479b2e666e424d8
mukhulaazam <mukhulaazam@gmail.com>
2019-03-08 09:15:02 +0600

    Team members section removed and next is redesign team members section

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-// $(document).ready(function() {
-// 	"use strict";
-// 	$(window).load(function() {
-// 		$(".preloader-container").fadeOut();
-// 		$(".preloader").delay(1000).fadeOut("slow");
-// 	});
+$(document).ready(function() {
+	"use strict";
+	// $(window).load(function() {
+	// 	$(".preloader-container").fadeOut();
+	// 	$(".preloader").delay(1000).fadeOut("slow");
+	// });
 
-// 	/*	------------------------------------
-// 			About us page section
-// 	----------------------------------------   */
-// 	// Skilled bar section started 
-// 	$('.skillbar').each(function(){
-// 		$(this).find('.skillbar-bar').animate({
-// 			width:$(this).attr('data-percent')
-// 		},3000);
-// 	});
-// 	// Skilled bar section end
-// });
+	// /*	------------------------------------
+	// 		About us page section
+	// ----------------------------------------   */
+	// // Skilled bar section started 
+	// $('.skillbar').each(function(){
+	// 	$(this).find('.skillbar-bar').animate({
+	// 		width:$(this).attr('data-percent')
+	// 	},3000);
+	// });
+	// Skilled bar section end
+});
 
