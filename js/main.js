a1488909b39d5b68552fd48d157745b83b3b9588
mukhulaazam <mukhulaazam@gmail.com>
2019-03-22 00:08:36 +0600

    CountDown section started and next is finisded

--- js/main.js
+++ js/main.js
@@ -16,6 +16,19 @@ $(document).ready(function() {
 	// });
 	// Skilled bar section end
 
+	
+	// Count Down section javaScript section
+	$('.do-count-number').each(function () {
+		$(this).prop('Counter',0).animate({
+			Counter: $(this).text()
+		}, {
+			duration: 6000,
+			easing: 'swing',
+			step: function (now) {
+				$(this).text(Math.ceil(now));
+			}
+		});
+	});
 		
 });
 
